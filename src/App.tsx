82144ba915724126085e6a00cd6a937134ac65df
Antonio <sago.ant@gmail.com>
2020-06-24 13:08:51 +0200

    feat: styled basic page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
-import { CssBaseline, ThemeProvider } from '@material-ui/core'
+import { CssBaseline, ThemeProvider, Container, makeStyles, createStyles, Typography } from '@material-ui/core'
 import theme from './theme'
 
-const App = () => (
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <div>Presumably useful page</div>
-  </ThemeProvider>
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    content: {
+      paddingTop: theme.spacing(8),
+      paddingBottom: theme.spacing(8),
+    },
+  }),
 )
+
+const App = () => {
+  const classes = useStyles()
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <Container className={classes.content} maxWidth="md">
+          <Typography component="h1" variant="h3" align="center">
+            Presumably useful page
+          </Typography>
+        </Container>
+    </ThemeProvider>
+  )
+}
 
 export default App;
