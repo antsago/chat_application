ee3a77a321525ec36b614141d3d88db08d52f388
Antonio <sago.ant@gmail.com>
2020-06-26 15:04:25 +0200

    feat: styled GoToButton

--- src/chat/GoToButton.tsx
+++ src/chat/GoToButton.tsx
@@ -1,12 +1,21 @@
 import React from "react"
-import { makeStyles, createStyles } from "@material-ui/core"
+import { makeStyles, createStyles, IconButton } from "@material-ui/core"
+import ArrowIcon from "@material-ui/icons/DoubleArrowRounded"
 import { GoToMessage } from "./MessengerHooks"
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme) =>
   createStyles({
-    root: {
-      height: "calc(100vh - 175px)",
-      overflow: "scroll",
+    button: {
+      position: "absolute",
+      bottom: 80,
+      right: 10,
+    },
+    icon: {
+      backgroundColor: theme.palette.primary.dark,
+      color: theme.palette.common.white,
+      transform: "rotate(90deg)",
+      borderRadius: "50%",
+      padding: "2px",
     },
   }),
 )
@@ -15,8 +24,19 @@ interface Props {
   onClick: GoToMessage
 }
 
-const GoToButton = ({ onClick }: Props): JSX.Element => (
-  <button onClick={onClick} type="button">Hi!</button>
-)
+const GoToButton = ({ onClick }: Props): JSX.Element => {
+  const classes = useStyles()
+
+  return (
+    <IconButton
+      className={classes.button}
+      onClick={onClick}
+      aria-label="Go to unread message"
+      type="button"
+    >
+      <ArrowIcon className={classes.icon} />
+    </IconButton>
+  )
+}
 
 export default GoToButton
--- src/chat/Header.tsx
+++ src/chat/Header.tsx
@@ -34,7 +34,9 @@ const ChatHeader = ({ unreadMessagesNo, onClick }: Props): JSX.Element => {
       role="button"
       tabIndex={0}
     >
-      <Typography className={classes.title} component="h2">Chat</Typography>
+      <Typography className={classes.title} component="h2">
+        Chat
+      </Typography>
 
       <Badge badgeContent={unreadMessagesNo} color="secondary">
         <ChatIcon fontSize="small" />
